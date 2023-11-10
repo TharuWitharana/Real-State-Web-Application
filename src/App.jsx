5016ce3dd104ab8580452ff8c0107379df8ebd77
TharuWitharana <tharushiwitharana@gmail.com>
2023-11-10 20:27:25 +0530

    update pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import SignIn from './pages/Signin';
 import SignUp from './pages/SignUp';
-import About from './pages/about';
+import About from './pages/About';
 import Profile from './pages/Profile';
 import Home from './pages/Home';
+import Header from './components/Header';
 
 export default function App() {
   return (
     <BrowserRouter>
+    <Header/>
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/sign-in" element={<SignIn />}></Route>
